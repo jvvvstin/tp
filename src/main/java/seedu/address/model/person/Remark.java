9b4c6067db4a3b0b9e886b8565004f2f60a00dd4
Justin <justin80@live.com.sg>
2025-09-19 23:23:42 +0800

    Add Remark class and change String remarks to Remark type

---
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -0,0 +1,33 @@
+package seedu.address.model.person;
+
+import static java.util.Objects.requireNonNull;
+
+/**
+ * Represents a Person's remark in the address book.
+ * Guarantees: immutable; is always valid
+ */
+public class Remark {
+    public final String value;
+
+    public Remark(String remark) {
+        requireNonNull(remark);
+        this.value = remark;
+    }
+
+    @Override
+    public String toString() {
+        return this.value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof Remark) // instanceof handles nulls
+                && value.equals(((Remark) other).value);
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+}
