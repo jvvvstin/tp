f309c8a1d3f31bdef12ef951c339fdedd37fc1c3
Justin <justin80@live.com.sg>
2025-10-03 23:23:15 +0800

    Add unit tests for Meeting class

---
+++ src/test/java/seedu/address/model/person/MeetingTest.java
@@ -0,0 +1,26 @@
+package seedu.address.model.person;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class MeetingTest {
+    @Test
+    public void equals() {
+        Meeting meeting = new Meeting("meeting", "venue", "datetime");
+
+        assertTrue(meeting.equals(meeting));
+
+        Meeting sameMeeting = new Meeting(meeting.meetingName, meeting.venue, meeting.when);
+        assertTrue(meeting.equals(sameMeeting));
+
+        assertFalse(meeting.equals(1));
+
+        assertFalse(meeting.equals(null));
+
+        Meeting differentMeeting = new Meeting("another meeting", "another venue",
+                "another datetime");
+        assertFalse(meeting.equals(differentMeeting));
+    }
+}
