2949eeb632a085d574721406a497d1650af048ce
Justin <justin80@live.com.sg>
2025-10-03 21:09:07 +0800

    Add tests for addmeeting command and parsing

---
+++ src/test/java/seedu/address/logic/commands/AddMeetingCommandTest.java
@@ -0,0 +1,21 @@
+package seedu.address.logic.commands;
+
+import static seedu.address.logic.commands.AddMeetingCommand.MESSAGE_NOT_IMPLEMENTED_YET;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+
+public class AddMeetingCommandTest {
+
+    private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+
+    @Test
+    public void exectute() {
+        assertCommandFailure(new AddMeetingCommand(), model, MESSAGE_NOT_IMPLEMENTED_YET);
+    }
+}
