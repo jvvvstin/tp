818c40874c5d3280a56a2908de44990acdd2c6be
Justin <justin80@live.com.sg>
2025-10-04 02:37:47 +0800

    Implement AddMeetingCommand execute() logic

--- src/main/java/seedu/address/logic/commands/AddMeetingCommand.java
+++ src/main/java/seedu/address/logic/commands/AddMeetingCommand.java
@@ -4,11 +4,17 @@ import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_MEETING;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_VENUE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_WHEN;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import seedu.address.commons.core.index.Index;
+import seedu.address.logic.Messages;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.meeting.Meeting;
+import seedu.address.model.person.Person;
 
 /**
  * Adds a meeting to an existing person in the address book.
@@ -28,7 +34,8 @@ public class AddMeetingCommand extends Command {
             + PREFIX_VENUE + "AMK Hub "
             + PREFIX_WHEN + "2025-11-01 1600";
 
-    public static final String MESSAGE_ARGUMENTS = "Index: %1$d, Meeting: %2$s";
+    public static final String MESSAGE_ADD_MEETING_SUCCESS = "Added meeting to Person: %1$s";
+    public static final String MESSAGE_ADD_MEETING_FAILURE = "Failed to add meeting to Person: %1$s";
 
     private final Index index;
     private final Meeting meeting;
@@ -42,7 +49,27 @@ public class AddMeetingCommand extends Command {
 
     @Override
     public CommandResult execute(Model model) throws CommandException {
-        throw new CommandException(String.format(MESSAGE_ARGUMENTS, index.getOneBased(), meeting));
+        List<Person> lastShownList = model.getFilteredPersonList();
+
+        if (index.getZeroBased() >= lastShownList.size()) {
+            throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+        }
+
+        Person personToEdit = lastShownList.get(index.getZeroBased());
+        List<Meeting> editedMeetings = new ArrayList<>(personToEdit.getMeetings());
+        editedMeetings.add(meeting);
+        Person editedPerson = new Person(personToEdit.getName(), personToEdit.getPhone(), personToEdit.getEmail(),
+                personToEdit.getAddress(), personToEdit.getTags(), editedMeetings);
+        model.setPerson(personToEdit, editedPerson);
+        model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
+
+        return new CommandResult(generateSuccessMessage(editedPerson));
+    }
+
+    private String generateSuccessMessage(Person personToEdit) {
+        String message = !(meeting.meetingName.isEmpty() || meeting.venue.isEmpty() || meeting.when.isEmpty()) ?
+                MESSAGE_ADD_MEETING_SUCCESS : MESSAGE_ADD_MEETING_FAILURE;
+        return String.format(message, Messages.format(personToEdit));
     }
 
     @Override
--- src/test/java/seedu/address/logic/commands/AddMeetingCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddMeetingCommandTest.java
@@ -2,7 +2,6 @@ package seedu.address.logic.commands;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static seedu.address.logic.commands.AddMeetingCommand.MESSAGE_ARGUMENTS;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_MEETING_AMY;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_MEETING_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_VENUE_AMY;
@@ -10,52 +9,87 @@ import static seedu.address.logic.commands.CommandTestUtil.VALID_VENUE_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_WHEN_AMY;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_WHEN_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.logic.commands.CommandTestUtil.showPersonAtIndex;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 import org.junit.jupiter.api.Test;
 
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.Messages;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
 import seedu.address.model.meeting.Meeting;
+import seedu.address.model.person.Person;
+import seedu.address.testutil.PersonBuilder;
 
 public class AddMeetingCommandTest {
 
+    private static final String MEETING_NAME_STUB = "Some meeting";
+    private static final String MEETING_VENUE_STUB = "Some venue";
+    private static final String MEETING_WHEN_STUB = "Some datetime";
     private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
 
     @Test
-    public void exectute() {
-        final String meetingName = "Some meeting";
-        final String venue = "Some venue";
-        final String when = "Some datetime";
-        final Meeting meeting = new Meeting(meetingName, venue, when);
-        System.out.println();
+    public void execute_addMeetingUnfilteredList_success() {
+        Person firstPerson = model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased());
+        Person editedPerson = new PersonBuilder(firstPerson).withMeetings(
+                new Meeting(MEETING_NAME_STUB, MEETING_VENUE_STUB, MEETING_WHEN_STUB)).build();
 
-        assertCommandFailure(new AddMeetingCommand(INDEX_FIRST_PERSON, meeting), model,
-                String.format(MESSAGE_ARGUMENTS, INDEX_FIRST_PERSON.getOneBased(), meeting));
+        AddMeetingCommand addMeetingCommand = new AddMeetingCommand(INDEX_FIRST_PERSON,
+                new Meeting(MEETING_NAME_STUB, MEETING_VENUE_STUB, MEETING_WHEN_STUB));
+
+        String expectedMessage = String.format(AddMeetingCommand.MESSAGE_ADD_MEETING_SUCCESS,
+                Messages.format(editedPerson));
+
+        Model expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
+        expectedModel.setPerson(firstPerson, editedPerson);
+
+        assertCommandSuccess(addMeetingCommand, model, expectedMessage, expectedModel);
     }
 
     @Test
-    public void equals() {
-        final AddMeetingCommand standardAddMeetingCommand = new AddMeetingCommand(INDEX_FIRST_PERSON,
-                new Meeting(VALID_MEETING_AMY, VALID_VENUE_AMY, VALID_WHEN_AMY));
+    public void execute_filteredList_success() {
+        showPersonAtIndex(model, INDEX_FIRST_PERSON);
 
-        AddMeetingCommand commandWithSameValues = new AddMeetingCommand(INDEX_FIRST_PERSON,
-                new Meeting(VALID_MEETING_AMY, VALID_VENUE_AMY, VALID_WHEN_AMY));
-        assertTrue(standardAddMeetingCommand.equals(commandWithSameValues));
+        Person firstPerson = model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased());
+        Person editedPerson = new PersonBuilder(model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased()))
+                .withMeetings(new Meeting(MEETING_NAME_STUB, MEETING_VENUE_STUB, MEETING_WHEN_STUB)).build();
 
-        assertTrue(standardAddMeetingCommand.equals(standardAddMeetingCommand));
+        AddMeetingCommand addMeetingCommand = new AddMeetingCommand(INDEX_FIRST_PERSON,
+                new Meeting(MEETING_NAME_STUB, MEETING_VENUE_STUB, MEETING_WHEN_STUB));
 
-        assertFalse(standardAddMeetingCommand.equals(null));
+        String expectedMessage = String.format(AddMeetingCommand.MESSAGE_ADD_MEETING_SUCCESS,
+                Messages.format(editedPerson));
 
-        assertFalse(standardAddMeetingCommand.equals(new ListCommand()));
+        Model expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
+        expectedModel.setPerson(firstPerson, editedPerson);
 
-        assertFalse(standardAddMeetingCommand.equals(new AddMeetingCommand(INDEX_SECOND_PERSON,
-                new Meeting(VALID_MEETING_AMY, VALID_VENUE_AMY, VALID_WHEN_AMY))));
+        assertCommandSuccess(addMeetingCommand, model, expectedMessage, expectedModel);
+    }
 
-        assertFalse(standardAddMeetingCommand.equals(new AddMeetingCommand(INDEX_FIRST_PERSON,
-                new Meeting(VALID_MEETING_BOB, VALID_VENUE_BOB, VALID_WHEN_BOB))));
+    @Test
+    public void execute_invalidPersonIndexUnfilteredList_failure() {
+        Index outOfBoundIndex = Index.fromOneBased(model.getFilteredPersonList().size() + 1);
+        AddMeetingCommand addMeetingCommand = new AddMeetingCommand(outOfBoundIndex,
+                new Meeting(MEETING_NAME_STUB, MEETING_VENUE_STUB, MEETING_WHEN_STUB));
+
+        assertCommandFailure(addMeetingCommand, model, Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+    }
+
+    @Test
+    public void execute_invalidPersonIndexFilteredList_failure() {
+        showPersonAtIndex(model, INDEX_FIRST_PERSON);
+        Index outOfBoundIndex = INDEX_SECOND_PERSON;
+
+        assertTrue(outOfBoundIndex.getZeroBased() < model.getAddressBook().getPersonList().size());
+
+        AddMeetingCommand addMeetingCommand = new AddMeetingCommand(outOfBoundIndex,
+                new Meeting(MEETING_NAME_STUB, MEETING_VENUE_STUB, MEETING_WHEN_STUB));
+
+        assertCommandFailure(addMeetingCommand, model, Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
     }
 }
