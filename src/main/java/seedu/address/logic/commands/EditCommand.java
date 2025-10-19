018bdceee03338ff7365a0ab946ac95cf5122886
MooMooYonders <181126455+MooMooYonders@users.noreply.github.com>
2025-10-19 15:06:06 +0800

    Update isAnyFieldEdited method to check isFlagged

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -171,7 +171,7 @@ public class EditCommand extends Command {
          * Returns true if at least one field is edited.
          */
         public boolean isAnyFieldEdited() {
-            return CollectionUtil.isAnyNonNull(name, phone, otherPhones, email, address, tags);
+            return CollectionUtil.isAnyNonNull(name, phone, otherPhones, email, address, tags, isFlagged);
         }
 
         public void setName(Name name) {
