a677bf54856b30bbd8af039fad1b412ad470ff57
MooMooYonders <181126455+MooMooYonders@users.noreply.github.com>
2025-10-20 15:37:45 +0800

    Add empty args check

--- src/test/java/seedu/address/logic/parser/FlagCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/FlagCommandParserTest.java
@@ -20,6 +20,7 @@ public class FlagCommandParserTest {
 
     @Test
     public void parse_invalidArgs_throwsParseException() {
+        assertParseFailure(parser, "", String.format(MESSAGE_INVALID_COMMAND_FORMAT, FlagCommand.MESSAGE_USAGE));
         assertParseFailure(parser, "a", String.format(MESSAGE_INVALID_COMMAND_FORMAT, FlagCommand.MESSAGE_USAGE));
     }
 
