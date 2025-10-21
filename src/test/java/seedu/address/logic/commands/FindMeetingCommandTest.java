3957db7f65b5f3a28b4a6dfc1e40b616259b80da
Zisaac99 <zisaac2468@gmail.com>
2025-10-22 00:20:20 +0800

    Add in unit/integration test

---
+++ src/test/java/seedu/address/logic/commands/FindMeetingCommandTest.java
@@ -0,0 +1,91 @@
+package seedu.address.logic.commands;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.logic.Messages.MESSAGE_PERSONS_LISTED_OVERVIEW;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.testutil.TypicalPersons.CARL;
+import static seedu.address.testutil.TypicalPersons.ELLE;
+import static seedu.address.testutil.TypicalPersons.FIONA;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+import seedu.address.model.person.MeetingNameContainsKeywordsPredicate;
+
+/**
+ * Contains integration tests (interaction with the Model) for {@code FindMeetingCommand}.
+ */
+public class FindMeetingCommandTest {
+    private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+    private Model expectedModel = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+
+    @Test
+    public void equals() {
+        MeetingNameContainsKeywordsPredicate firstPredicate =
+                new MeetingNameContainsKeywordsPredicate(Collections.singletonList("first"));
+        MeetingNameContainsKeywordsPredicate secondPredicate =
+                new MeetingNameContainsKeywordsPredicate(Collections.singletonList("second"));
+
+        FindMeetingCommand findFirstCommand = new FindMeetingCommand(firstPredicate);
+        FindMeetingCommand findSecondCommand = new FindMeetingCommand(secondPredicate);
+
+        // same object -> returns true
+        assertTrue(findFirstCommand.equals(findFirstCommand));
+
+        // same values -> returns true
+        FindMeetingCommand findFirstCommandCopy = new FindMeetingCommand(firstPredicate);
+        assertTrue(findFirstCommand.equals(findFirstCommandCopy));
+
+        // different types -> returns false
+        assertFalse(findFirstCommand.equals(1));
+
+        // null -> returns false
+        assertFalse(findFirstCommand.equals(null));
+
+        // different person -> returns false
+        assertFalse(findFirstCommand.equals(findSecondCommand));
+    }
+
+    @Test
+    public void execute_zeroKeywords_noPersonFound() {
+        String expectedMessage = String.format(MESSAGE_PERSONS_LISTED_OVERVIEW, 0);
+        MeetingNameContainsKeywordsPredicate predicate = preparePredicate(" ");
+        FindMeetingCommand command = new FindMeetingCommand(predicate);
+        expectedModel.updateFilteredPersonList(predicate);
+        assertCommandSuccess(command, model, expectedMessage, expectedModel);
+        assertEquals(Collections.emptyList(), model.getFilteredPersonList());
+    }
+
+    @Test
+    public void execute_multipleKeywords_multiplePersonsFound() {
+        String expectedMessage = String.format(MESSAGE_PERSONS_LISTED_OVERVIEW, 3);
+        MeetingNameContainsKeywordsPredicate predicate = preparePredicate("zoom google teams");
+        FindMeetingCommand command = new FindMeetingCommand(predicate);
+        expectedModel.updateFilteredPersonList(predicate);
+        assertCommandSuccess(command, model, expectedMessage, expectedModel);
+        assertEquals(Arrays.asList(CARL, ELLE, FIONA), model.getFilteredPersonList());
+    }
+
+    @Test
+    public void toStringMethod() {
+        MeetingNameContainsKeywordsPredicate predicate = new MeetingNameContainsKeywordsPredicate(Arrays.asList("keyword"));
+        FindMeetingCommand FindMeetingCommand = new FindMeetingCommand(predicate);
+        String expected = FindMeetingCommand.class.getCanonicalName() + "{predicate=" + predicate + "}";
+        assertEquals(expected, FindMeetingCommand.toString());
+    }
+
+    /**
+     * Parses {@code userInput} into a {@code MeetingNameContainsKeywordsPredicate}.
+     */
+    private MeetingNameContainsKeywordsPredicate preparePredicate(String userInput) {
+        return new MeetingNameContainsKeywordsPredicate(Arrays.asList(userInput.split("\\s+")));
+    }
+}
---
+++ src/test/java/seedu/address/logic/parser/FindMeetingCommandParserTest.java
@@ -0,0 +1,38 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+
+import java.util.Arrays;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.logic.commands.FindMeetingCommand;
+import seedu.address.model.person.MeetingNameContainsKeywordsPredicate;
+
+public class FindMeetingCommandParserTest {
+
+    private FindMeetingCommandParser parser = new FindMeetingCommandParser();
+
+    @Test
+    public void parse_emptyArg_throwsParseException() {
+        String emptyArgMessage = "Your arguments should not be empty!\n";
+        String expectedExceptionMessage = String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                emptyArgMessage + FindMeetingCommand.MESSAGE_USAGE);
+        assertParseFailure(parser, "     ", expectedExceptionMessage);
+    }
+
+    @Test
+    public void parse_validArgs_returnsFindCommand() {
+        // no leading and trailing whitespaces
+        FindMeetingCommand expectedFindCommand =
+                new FindMeetingCommand(new MeetingNameContainsKeywordsPredicate(
+                        Arrays.asList("meeting", "afternoon")));
+        assertParseSuccess(parser, "meeting afternoon", expectedFindCommand);
+
+        // multiple whitespaces between keywords
+        assertParseSuccess(parser, " \n meeting \n \t afternoon  \t", expectedFindCommand);
+    }
+
+}
