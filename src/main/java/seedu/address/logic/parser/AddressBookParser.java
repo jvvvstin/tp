0005d3ea116c0e261357f9a08856dfd56fcb6473
Zisaac99 <zisaac2468@gmail.com>
2025-10-22 00:20:36 +0800

    Implement find meeting command

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -18,6 +18,7 @@ import seedu.address.logic.commands.EditCommand;
 import seedu.address.logic.commands.EditMeetingCommand;
 import seedu.address.logic.commands.ExitCommand;
 import seedu.address.logic.commands.FindCommand;
+import seedu.address.logic.commands.FindMeetingCommand;
 import seedu.address.logic.commands.HelpCommand;
 import seedu.address.logic.commands.ListCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
@@ -88,6 +89,9 @@ public class AddressBookParser {
 
         case EditMeetingCommand.COMMAND_WORD:
             return new EditMeetingCommandParser().parse(arguments);
+
+        case FindMeetingCommand.COMMAND_WORD:
+            return new FindMeetingCommandParser().parse(arguments);
 
         default:
             logger.finer("This user input caused a ParseException: " + userInput);
---
+++ src/main/java/seedu/address/logic/parser/FindMeetingCommandParser.java
@@ -0,0 +1,36 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+import java.util.Arrays;
+
+import seedu.address.logic.commands.FindMeetingCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.person.MeetingNameContainsKeywordsPredicate;
+
+/**
+ * Parses input arguments and creates a new FindMeetingCommand object
+ */
+public class FindMeetingCommandParser implements Parser<FindMeetingCommand> {
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the FindMeetingCommand
+     * and returns a FindMeetingCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public FindMeetingCommand parse(String args) throws ParseException {
+        String trimmedArgs = args.trim();
+
+        if (trimmedArgs.isEmpty()) {
+            String emptyArgMessage = "Your arguments should not be empty!\n";
+            String exceptionMessage = emptyArgMessage + FindMeetingCommand.MESSAGE_USAGE;
+
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, exceptionMessage));
+        }
+
+        String[] nameKeywords = trimmedArgs.split("\\s+");
+
+        return new FindMeetingCommand(new MeetingNameContainsKeywordsPredicate(Arrays.asList(nameKeywords)));
+    }
+
+}
