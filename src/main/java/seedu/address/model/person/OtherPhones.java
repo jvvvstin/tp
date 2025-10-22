e53608c191ba734b3d9d5748d6b4bf0383f43572
Nic_soh_lame <sohnicholas02@gmail.com>
2025-10-22 23:10:21 +0800

    Add otherPhones validations

--- src/main/java/seedu/address/model/person/OtherPhones.java
+++ src/main/java/seedu/address/model/person/OtherPhones.java
@@ -1,14 +1,29 @@
 package seedu.address.model.person;
 
+import seedu.address.commons.core.LogsCenter;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.logging.Logger;
+
+import seedu.address.commons.core.LogsCenter;
 import static seedu.address.commons.util.AppUtil.checkArgument;
+import static seedu.address.logic.parser.ParserUtil.parseParametersAndLabels;
+import static seedu.address.model.person.Person.LABEL_VALIDATION_REGEX;
 
 /**
  * Represents a Peron's other numbers in the address books.
  */
 public class OtherPhones {
+    private static final Logger logger = LogsCenter.getLogger(OtherPhones.class);
     public static final String MESSAGE_CONSTRAINTS =
             "Phone numbers should only contain numbers, and it should be at "
                    + "least 3 digits long and numbers should be separared by a tag e.g. 9999 (work) 8888 (office)";
+
+    public static final String MESSAGE_DUPLICATE_CONSTRAINTS =
+            "The main number already exists. Do not key in duplicate numbers";
 
     // Single phone number pattern
     private static final String SINGLE_PHONE_REGEX =
@@ -18,19 +33,26 @@ public class OtherPhones {
     private static final String MULTI_PHONE_REGEX =
             "^\\s*(?:(?:\\+\\d{1,3}\\s*)?\\d{3,}(?:\\s*x\\d+)?\\s*\\([^()]+\\)\\s*)+$";
 
-    public final String value;
+    public final String numbers;
+
 
     /**
      * Constructs a {@code OtherPhones}
      * @param phones Valid phone numbers with a tag each or a single phone number
      */
-    public OtherPhones(String phones) {
+    public OtherPhones(String phones)  {
         if (phones != null && !phones.equals("")) {
             phones = phones.trim();
-            checkArgument(isValidPhone(phones), MESSAGE_CONSTRAINTS);
+
+            try {
+                checkArgument(isValidPhone(phones), MESSAGE_CONSTRAINTS);
+            } catch (ParseException e){
+                logger.warning("ParseException thrown for phone constructor for: " + phones);
+            }
+
         }
 
-        this.value = phones;
+        this.numbers = phones;
     }
 
     /**
@@ -38,13 +60,85 @@ public class OtherPhones {
      * @param test
      * @return boolean
      */
-    public static boolean isValidPhone(String test) {
-        return test.matches(SINGLE_PHONE_REGEX) || test.matches(MULTI_PHONE_REGEX);
+    public static boolean isValidPhone(String test) throws ParseException {
+        String trimmedText = test.trim();
+        if (trimmedText.isEmpty()) {
+            return true;
+        }
+        List<String> paramsAndLabels = parseParametersAndLabels(
+                OtherPhones.class.getName().toLowerCase(),test, false);
+        return isPhonesAndLabelsValid(paramsAndLabels);
+    }
+
+    /**
+     * <p>
+     * Check if the parameters and labels of a Phone is valid.
+     * Phones and labels must be distinct they cannot be repeated.
+     * For it to be valid we accept:
+     * </p><br><p>
+     * 1) If there is only one email we accept either: Phone or Phone (LABEL)
+     * </p><p>
+     * 2) If there is more than one Phone every Phone must be accompanied by a label like: Phone1 (LABEL1)
+     * Phone2 (LABEL2) ...
+     * </p>
+     * @param phones The {@code List<String>} of the parameters and labels of a Phone.
+     * @return A boolean indicating if the parameters and labels of a Phone is valid.
+     */
+    public static boolean isPhonesAndLabelsValid(List<String> phones) {
+        boolean checkPhone = true;
+        Set<String> set = new HashSet<>();
+
+        for (String currString : phones) {
+            if (set.contains(currString)) {
+                return false;
+            }
+
+            if (checkPhone && !currString.matches(SINGLE_PHONE_REGEX)) {
+                return false;
+            }
+
+            if (!checkPhone && !currString.matches(LABEL_VALIDATION_REGEX)) {
+                return false;
+            }
+
+            set.add(currString);
+
+            checkPhone = !checkPhone;
+        }
+
+        return true;
+    }
+
+
+    /**
+     * Returns true if otherPhones contains the main number
+     * @param mainPhone, otherPhones
+     * @return boolean
+     */
+    public static boolean mainPhoneExists(String otherPhones, Phone mainPhone) throws ParseException {
+        otherPhones = otherPhones.trim();
+        if (otherPhones == null || otherPhones.equals("")) {
+            return false;
+        }
+
+        assert mainPhone != null : "Main Phone cannot be null: Main phone should have been created";
+        List<String> paramsAndLabels = parseParametersAndLabels(
+                OtherPhones.class.getName().toLowerCase(),otherPhones, false);
+
+        for (String currPhone : paramsAndLabels) {
+            if (!currPhone.matches(SINGLE_PHONE_REGEX)) {
+                continue;
+            }
+            if (mainPhone.equals(new Phone(currPhone))){
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override
     public String toString() {
-        return value;
+        return numbers;
     }
 
     @Override
@@ -58,12 +152,12 @@ public class OtherPhones {
         }
 
         OtherPhones otherPhone = (OtherPhones) other;
-        return value.equals(otherPhone.value);
+        return numbers.equals(otherPhone.numbers);
     }
 
     @Override
     public int hashCode() {
-        return value.hashCode();
+        return numbers.hashCode();
     }
 
 }
